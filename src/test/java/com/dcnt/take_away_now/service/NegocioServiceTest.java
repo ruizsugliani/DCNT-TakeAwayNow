7acf0d7769c9dbb0452e6415250297cb4e8dd744
Lautaro Ghosn <lghosn@fi.uba.ar>
2024-01-08 21:07:24 -0300

    ProductoRepositoryTest + NegocioServiceTest

--- src/test/java/com/dcnt/take_away_now/service/NegocioServiceTest.java
+++ src/test/java/com/dcnt/take_away_now/service/NegocioServiceTest.java
@@ -9,51 +9,73 @@ import com.dcnt.take_away_now.repository.NegocioRepository;
 import com.dcnt.take_away_now.repository.ProductoRepository;
 import com.dcnt.take_away_now.value_object.Dinero;
 import com.dcnt.take_away_now.value_object.PuntosDeConfianza;
+import org.apache.hc.core5.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.http.ResponseEntity;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.time.DayOfWeek;
 import java.util.Collection;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@ExtendWith(MockitoExtension.class)
+//@ExtendWith(MockitoExtension.class)
+@DataJpaTest
 class NegocioServiceTest {
-    @Mock
+    @Autowired
     private NegocioRepository negocioRepository;
-    @Mock
+    @Autowired
     private InventarioRegistroRepository inventarioRegistroRepository;
-    @Mock
+    @Autowired
     private ProductoRepository productoRepository;
     private NegocioService negocioService;
     private DayOfWeek DiaDeApertura;
     private DayOfWeek DiaDeCierre;
+    private int HoraApertura;
+    private int MinutoApertura;
+    private int HoraCierre;
+    private int MinutoCierre;
     @BeforeEach
     void setUp() {
         DiaDeApertura = DayOfWeek.MONDAY;
         DiaDeCierre = DayOfWeek.FRIDAY;
         negocioService = new NegocioService(negocioRepository, inventarioRegistroRepository, productoRepository);
+        HoraApertura = 9;
+        MinutoApertura = 0;
+        HoraCierre = 18;
+        MinutoApertura = 0;
     }
 
     @Test
-    void crearNegocio() {
+    void sePuedeCrearNegocioNuevo() {
         //when
-        negocioService.crearNegocio("Buffet Paseo Colon", DiaDeApertura, DiaDeCierre);
+        negocioService.crearNegocio("Buffet Paseo Colon", DiaDeApertura, DiaDeCierre,HoraApertura, MinutoApertura, HoraCierre, MinutoCierre);
         Optional<Negocio> negocio = negocioRepository.findByNombre("Buffet Paseo Colon");
         //then
         assertThat(negocio.isPresent()).isTrue();
     }
 
     @Test
-    void obtenerNegocios() {
+    void noSePuedeCrearNegocioExistente() {
         //given
-        negocioService.crearNegocio("Buffet Paseo Colon", DiaDeApertura, DiaDeCierre);
-        negocioService.crearNegocio("Buffet Las Heras", DiaDeApertura, DiaDeCierre);
-        negocioService.crearNegocio("Buffet Ciudad Universitaria", DiaDeApertura, DiaDeCierre);
+        negocioService.crearNegocio("Buffet Paseo Colon", DiaDeApertura, DiaDeCierre,HoraApertura, MinutoApertura, HoraCierre, MinutoCierre);
+        //when
+         ResponseEntity<HttpStatus> status = negocioService.crearNegocio("Buffet Paseo Colon", DiaDeApertura, DiaDeCierre,HoraApertura, MinutoApertura, HoraCierre, MinutoCierre);
+        //then
+        assertThat(status).isEqualTo(ResponseEntity.badRequest().build());
+    }
+
+    @Test
+    void seObtienenNegociosExistentes() {
+        //given
+        negocioService.crearNegocio("Buffet Paseo Colon", DiaDeApertura, DiaDeCierre,HoraApertura, MinutoApertura, HoraCierre, MinutoCierre);
+        negocioService.crearNegocio("Buffet Las Heras", DiaDeApertura, DiaDeCierre,HoraApertura, MinutoApertura, HoraCierre, MinutoCierre);
+        negocioService.crearNegocio("Buffet Ciudad Universitaria", DiaDeApertura, DiaDeCierre,HoraApertura, MinutoApertura, HoraCierre, MinutoCierre);
         //when
         Collection<Negocio> negocios = negocioService.obtenerNegocios();
         //then
@@ -61,9 +83,9 @@ class NegocioServiceTest {
     }
 
     @Test
-    void crearProducto() {
+    void sePuedeCrearProductoNuevoEnNegocioExistente() {
         //given
-        negocioService.crearNegocio("Buffet Paseo Colon", DiaDeApertura, DiaDeCierre);
+        negocioService.crearNegocio("Buffet Paseo Colon", DiaDeApertura, DiaDeCierre,HoraApertura, MinutoApertura, HoraCierre, MinutoCierre);
         InventarioRegistroDto inventarioRegistroDto = new InventarioRegistroDto(10L, new Dinero(100), new PuntosDeConfianza(20));
         Optional<Negocio> negocio = negocioRepository.findByNombre("Buffet Paseo Colon");
         //when
@@ -74,11 +96,31 @@ class NegocioServiceTest {
         assertThat(producto).isPresent();
         assertThat(existeInventarioRegistro).isTrue();
     }
-
+    @Test
+    void noSePuedeCrearProductoYaExistenteEnNegocioExistente() {
+        //given
+        negocioService.crearNegocio("Buffet Paseo Colon", DiaDeApertura, DiaDeCierre,HoraApertura, MinutoApertura, HoraCierre, MinutoCierre);
+        InventarioRegistroDto inventarioRegistroDto = new InventarioRegistroDto(10L, new Dinero(100), new PuntosDeConfianza(20));
+        Optional<Negocio> negocio = negocioRepository.findByNombre("Buffet Paseo Colon");
+        negocioService.crearProducto(negocio.get().getId(), "Pancho",inventarioRegistroDto);
+        //when
+        ResponseEntity<HttpStatus> status= negocioService.crearProducto(negocio.get().getId(), "Pancho",inventarioRegistroDto);
+        //then
+        assertThat(status).isEqualTo(ResponseEntity.internalServerError().build());
+    }
+    @Test
+    void noSePuedeCrearProductoNuevoEnNegocioQueNoExiste() {
+        //given
+        InventarioRegistroDto inventarioRegistroDto = new InventarioRegistroDto(10L, new Dinero(100), new PuntosDeConfianza(20));
+        //when
+        ResponseEntity<HttpStatus> status= negocioService.crearProducto(1L, "Pancho",inventarioRegistroDto);
+        //then
+        assertThat(status).isEqualTo(ResponseEntity.notFound().build());
+    }
     @Test
     void eliminarProducto() {
         //given
-        negocioService.crearNegocio("Buffet Paseo Colon", DiaDeApertura, DiaDeCierre);
+        negocioService.crearNegocio("Buffet Paseo Colon", DiaDeApertura, DiaDeCierre,HoraApertura, MinutoApertura, HoraCierre, MinutoCierre);
         InventarioRegistroDto inventarioRegistroDto = new InventarioRegistroDto(10L, new Dinero(100), new PuntosDeConfianza(20));
         Optional<Negocio> negocio = negocioRepository.findByNombre("Buffet Paseo Colon");
         negocioService.crearProducto(negocio.get().getId(), "Pancho",inventarioRegistroDto);
@@ -96,7 +138,7 @@ class NegocioServiceTest {
     @Test
     void obtenerProductos() {
         //given
-        negocioService.crearNegocio("Buffet Paseo Colon", DiaDeApertura, DiaDeCierre);
+        negocioService.crearNegocio("Buffet Paseo Colon", DiaDeApertura, DiaDeCierre,HoraApertura, MinutoApertura, HoraCierre, MinutoCierre);
         InventarioRegistroDto inventarioRegistroDto = new InventarioRegistroDto(10L, new Dinero(100), new PuntosDeConfianza(20));
         Optional<Negocio> negocio = negocioRepository.findByNombre("Buffet Paseo Colon");
         negocioService.crearProducto(negocio.get().getId(), "Pancho",inventarioRegistroDto);
@@ -107,4 +149,17 @@ class NegocioServiceTest {
         //then
         assertThat(productos.size()).isEqualTo(3);
     }
+    @Test
+    void noSePuedeobtenerProductosDeUnNegocioQueNoExiste() {
+        // when: "Se intenta obtener productos de un negocio que no existe"
+        assertThatThrownBy(
+                () -> {
+                    negocioService.obtenerProductos(1L);
+                }
+        )
+        // then: "se lanza error"
+        .isInstanceOf(NoSuchElementException.class)
+        .hasMessageContaining("No existe el negocio al cual se solicitó obtener sus productos.");
+    }
+
 }
